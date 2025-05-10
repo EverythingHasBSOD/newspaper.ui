68e41fedde2c5dd0118892c41376c764f4fdbcba
EverythingHasBSOD <qasimlimurad92@gmail.com>
2025-05-10 21:22:15 +0400

    index

---
+++ app.js
@@ -0,0 +1,13 @@
+function func_webkit () {
+    document.getElementById('charset').innerHTML='©BSOD11'
+}
+
+function func_innerhtml () {
+    document.getElementById('classid').innerHTML='Unordered List:'
+}
+
+function func_text () {
+      document.getElementById('clsid').innerHTML='Ordered List:'
+}
+
+
